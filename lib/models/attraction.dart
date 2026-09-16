039269146a56d19553b600308e502e7a7d8b6d95
Darshan Kumar <darshanskull@gmail.com>
2026-09-16 15:13:58 +0530

    Add Firestore mapping and map coordinates to Attraction model

--- lib/models/attraction.dart
+++ lib/models/attraction.dart
@@ -10,6 +10,8 @@ class Attraction {
     required this.location,
     required this.openingHours,
     required this.entryFee,
+    this.latitude,
+    this.longitude,
     this.isSaved = false,
   });
 
@@ -23,7 +25,44 @@ class Attraction {
   final String location;
   final String openingHours;
   final String entryFee;
+  final double? latitude;
+  final double? longitude;
   final bool isSaved;
+
+  factory Attraction.fromFirestore(String id, Map<String, dynamic> data) {
+    return Attraction(
+      id: id,
+      name: data['name'] as String? ?? 'Unnamed attraction',
+      category: data['category'] as String? ?? 'Other',
+      description: data['description'] as String? ?? '',
+      imageUrl: data['imageUrl'] as String? ?? '',
+      rating: (data['rating'] as num?)?.toDouble() ?? 0,
+      distance: data['distance'] as String? ?? 'Nearby',
+      location: data['location'] as String? ?? '',
+      openingHours: data['openingHours'] as String? ?? 'Not available',
+      entryFee: data['entryFee'] as String? ?? 'Not available',
+      latitude: (data['latitude'] as num?)?.toDouble(),
+      longitude: (data['longitude'] as num?)?.toDouble(),
+      isSaved: data['isSaved'] as bool? ?? false,
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'name': name,
+      'category': category,
+      'description': description,
+      'imageUrl': imageUrl,
+      'rating': rating,
+      'distance': distance,
+      'location': location,
+      'openingHours': openingHours,
+      'entryFee': entryFee,
+      'latitude': latitude,
+      'longitude': longitude,
+      'isSaved': isSaved,
+    };
+  }
 
   Attraction copyWith({bool? isSaved}) {
     return Attraction(
@@ -37,6 +76,8 @@ class Attraction {
       location: location,
       openingHours: openingHours,
       entryFee: entryFee,
+      latitude: latitude,
+      longitude: longitude,
       isSaved: isSaved ?? this.isSaved,
     );
   }
